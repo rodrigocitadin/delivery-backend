2b33c0245e0a93136886e518f1cc097b5b2404be
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-25 17:43:35 -0300

    feat: create deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,32 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateDeliveryman {
+  username: string,
+  password: string
+};
+
+export class CreateDeliverymanUseCase {
+  async execute({ password, username }: ICreateDeliveryman) {
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username
+      }
+    })
+
+    if (deliverymanExist) {
+      throw new Error("deliveryman already exists");
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    });
+
+    return deliveryman;
+  }
+};
