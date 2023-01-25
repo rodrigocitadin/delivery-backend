1b491f2aeb1ebb117f11b1383d3f34c1c71fb8f6
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-25 19:54:57 -0300

    feat: create delivery route

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from "./modules/clients/useCases/createClient/
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 
 const routes = Router();
 
@@ -10,12 +11,14 @@ const createClientController = new CreateClientController;
 const authenticateClientController = new AuthenticateClientController;
 const createDeliverymanController = new CreateDeliverymanController
 const authenticateDeliverymanController = new AuthenticateDeliverymanController;
-
+const createDeliveryController = new CreateDeliveryController;
 
 routes.post("/client/", createClientController.handle);
 routes.post("/client/authenticate/", authenticateClientController.handle);
 
 routes.post("/deliveryman/", createDeliverymanController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
+
+routes.post("/delivery/", createDeliveryController.handle);
 
 export { routes };
