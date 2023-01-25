f33a0b7d95149aae39172bfb938948c581e850a4
rodrigocitadin <citadinrodrigo@gmail.com>
2023-01-25 17:53:27 -0300

    feat: deliveryman authenticate route

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
-import { AuthenticateClientController } from "./modules/account/authenticateUser/AuthenticateClientController";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController;
 const authenticateClientController = new AuthenticateClientController;
 const createDeliverymanController = new CreateDeliverymanController
+const authenticateDeliverymanController = new AuthenticateDeliverymanController;
+
 
 routes.post("/client/", createClientController.handle);
-routes.post("/authenticate/", authenticateClientController.handle);
+routes.post("/client/authenticate/", authenticateClientController.handle);
+
 routes.post("/deliveryman/", createDeliverymanController.handle);
+routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
 
 export { routes };
